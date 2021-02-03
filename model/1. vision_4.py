e7304be30c6cfc2440e89a13b3dc7a767d63a014
Jeong kyuhyun <jare92n@gmail.com>
2021-02-03 22:22:04 +0900

    210203 baseline

--- model/1. vision_4.py
+++ model/1. vision_4.py
@@ -1,100 +1,160 @@
-import pandas as pd
+## y 값은 digit 지금 y 값이 주어지지 않음
+## 따로 y값 나누는 거 아니다
+### pca 안한게 10배는 잘나온다
+
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+from keras.preprocessing.image import ImageDataGenerator # 이미지데이터 늘리는 작업
+from numpy import expand_dims
+from sklearn.model_selection import StratifiedKFold
+from keras import Sequential,Model
+from keras.layers import *
+from sklearn.decomposition import PCA
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras.optimizers import Adam
+from tensorflow.keras.layers import Dense, MaxPooling2D, Conv2D, Flatten, MaxPooling1D, Conv1D
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
 import warnings
-warnings.filterwarnings("ignore")
-import tensorflow as tf
+warnings.filterwarnings('ignore')
 
 train = pd.read_csv('./csv/train.csv')
 test = pd.read_csv('./csv/test.csv')
 
-x_train = train.drop(['id', 'letter'], axis=1)
+# ==============  데이터 & 전처리  =========================
+# print(train,test,sub)
+# # distribution of label('digit') 
+tra_di = train['digit'].value_counts()
 
-def select_index(column, x):
-    a = x_train[x_train[column].isin([x])]
-    x = a.iloc[:,1:]
-    y = a.iloc[:,0]
-    x = x.values.reshape(-1,28,28,1)
-    x = x/255.0
-    return x, y
+# drop 인덱스
+train2 = train.drop(['id','digit','letter'],1) # 인덱스 있는 3개 버리기
+test2 = test.drop(['id','letter'],1) #인덱스 있는 것 버리기
 
-x0, y0 = select_index("digit", "0")
-x1, y1 = select_index("digit", "1")
-x2, y2 = select_index("digit", "2")
-x3, y3 = select_index("digit", "3")
-x4, y4 = select_index("digit", "4")
-x5, y5 = select_index("digit", "5")
-x6, y6 = select_index("digit", "6")
-x7, y7 = select_index("digit", "7")
-x8, y8 = select_index("digit", "8")
-x9, y9 = select_index("digit", "9")
+# convert pandas dataframe to numpy array
+train2 = train2.values
+test2 = test2.values
+# print(train2.shape) #(2048, 784)
+# print(test2.shape) # (20480, 784)
 
+# 정규화(Minmax도 해보기) ---> standard보다 Minmax가 잘나온다
+scaler = MinMaxScaler()
+scaler.fit(train2)
+scaler.transform(train2)
+scaler.transform(test2)
 
-'''
-x_train = x_train.reshape(-1, 28, 28, 1)
-x = x_train/255
-y = train['digit']
+# # reshape
+train2 = train2.reshape(-1,28,28,1)
+test2 = test2.reshape(-1,28,28,1)
+# train2 = train2.reshape(-1,97,2,1)
+# test2 = test2.reshape(-1,97,2,1) #4차원
 
-from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test= train_test_split(x, y, train_size = 0.8, shuffle = True, random_state = 66 )
+# ImageDatagenerator & data augmentation
+idg = ImageDataGenerator(height_shift_range=(-1,1),width_shift_range=(-1,1)) # 이미지 카테고리화(4차원만 가능)
+idg2 = ImageDataGenerator() #ImageDataGenerator 머신러닝
+# width_shift_range 좌우로 움직이는 정도:(-1,1) 처음부터 끝까지
+# height_shift_range 위아래로 움직이는 정도
 
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-encoder = OneHotEncoder()
-y_train = encoder.fit_transform(y_train.values.reshape(-1,1)).toarray()
-y_test = encoder.fit_transform(y_test.values.reshape(-1,1)).toarray()
-# y_train = np.zeros((len(y), len(y.unique())))
-# for i, digit in enumerate(y):
-#     y_train[i, digit] = 1
+# ================== 모델링 ==============================
+def modeling() :
+    model = Sequential()
+    model.add(Conv2D(16,(3,3),activation='relu',input_shape=(28,28,1),padding='same'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.3))
+    
+    model.add(Conv2D(32,(3,3),activation='relu',padding='same'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(32,(5,5),activation='relu',padding='same')) 
+    model.add(BatchNormalization())
+    model.add(Conv2D(32,(5,5),activation='relu',padding='same'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(32,(5,5),activation='relu',padding='same'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D((3,3)))
+    model.add(Dropout(0.3))
+    
+    model.add(Flatten()) #2차원
+    model.add(Dense(128,activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.3))
+    model.add(Dense(64,activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dense(32,activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.3))
+    model.add(Dense(10,activation='softmax')) # softmax는 'categorical_crossentropy' 짝꿍
+    return model
+    # inputs1 = Input(shape=(28,28,1))
+    # bn = BatchNormalization(trainable=False)(inputs1)                                      # 배치정규화
+    # conv = Conv2D(128, kernel_size=5, strides=1, padding='same', activation='relu')(bn)   # CNN
+    # bn = BatchNormalization()(conv)                                                       # 배치정규화
+    # conv = Conv2D(128, kernel_size=2, strides=1, padding='same', activation='relu')(bn)   # CNN
+    # pool = MaxPooling2D((2, 2))(conv)                                                     # Max Pooling
 
+    # bn = BatchNormalization()(pool)                                                       # 배치정규화
+    # conv = Conv2D(256, kernel_size=2, strides=1, padding='same', activation='relu')(bn)   # CNN
+    # bn = BatchNormalization()(conv)                                                       # 배치정규화
+    # conv = Conv2D(256, kernel_size=2, strides=1, padding='same', activation='relu')(bn)   # CNN
+    # pool = MaxPooling2D((2, 2))(conv)                                                     # Max Pooling
 
-from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout, Conv1D, MaxPool1D, LSTM, Input, BatchNormalization,MaxPooling2D
-inputs1 = Input(shape=(28,28,1))
-bn = BatchNormalization(trainable=False)(inputs1)                                      # 배치정규화
-conv = Conv2D(128, kernel_size=5, strides=1, padding='same', activation='relu')(bn)   # CNN
-bn = BatchNormalization()(conv)                                                       # 배치정규화
-conv = Conv2D(128, kernel_size=2, strides=1, padding='same', activation='relu')(bn)   # CNN
-pool = MaxPooling2D((2, 2))(conv)                                                     # Max Pooling
+    # bn = BatchNormalization()(pool)                                                       # 배치정규화
+    # conv = Conv2D(512, kernel_size=2, strides=1, padding='same', activation='relu')(bn)   # CNN
+    # bn = BatchNormalization()(conv)                                                       # 배치정규화
+    # conv = Conv2D(512, kernel_size=2, strides=1, padding='same', activation='relu')(bn)   # CNN
+    # pool = MaxPooling2D((2, 2))(conv) 
 
-bn = BatchNormalization()(pool)                                                       # 배치정규화
-conv = Conv2D(256, kernel_size=2, strides=1, padding='same', activation='relu')(bn)   # CNN
-bn = BatchNormalization()(conv)                                                       # 배치정규화
-conv = Conv2D(256, kernel_size=2, strides=1, padding='same', activation='relu')(bn)   # CNN
-pool = MaxPooling2D((2, 2))(conv)                                                     # Max Pooling
+    # flatten = Flatten()(pool)                                                             # Flatten
+    # bn = BatchNormalization()(flatten)                                                    # 배치정규화
+    # dense = Dense(1000, activation='relu')(bn)                                            # Fully Connected Layer
+    # bn = BatchNormalization()(dense)                                                      # 배치정규화
+    # outputs = Dense(10, activation='softmax')(bn)   
+    # model = Model(inputs=inputs1, outputs=outputs)
+    # return model
+    
+re = ReduceLROnPlateau(patience=50, verbose=1, factor= 0.5)
+ea = EarlyStopping(patience=100, verbose=1, mode='auto')
+epochs = 1000
+#KFlod대신 StratifiedKFold 써보기
+#stratified 는 label 의 분포를 유지, 각 fold가 전체 데이터셋을 잘 대표한다.
+skf = StratifiedKFold(n_splits=15, random_state=42, shuffle=True) #n_splits 몇 번 반복
+val_loss_min = []
+result = 0
+nth = 0
+t_d = train['digit'] # y 값 부여
 
-bn = BatchNormalization()(pool)                                                       # 배치정규화
-conv = Conv2D(512, kernel_size=2, strides=1, padding='same', activation='relu')(bn)   # CNN
-bn = BatchNormalization()(conv)                                                       # 배치정규화
-conv = Conv2D(512, kernel_size=2, strides=1, padding='same', activation='relu')(bn)   # CNN
-pool = MaxPooling2D((2, 2))(conv) 
+for train_index, valid_index in skf.split(train2, t_d):
+    x_train = train2[train_index]
+    x_valid = train2[valid_index]
+    y_train = t_d[train_index]
+    y_valid = t_d[valid_index]
+    # print(x_train.shape, x_valid.shape) #(1946, 28, 28, 1), (102, 28, 28, 1)
+    # print(y_train.shape, y_valid.shape) #(1946,) (102,)
 
+    # 실시간 데이터 증강을 사용해 배치에 대해서 모델을 학습(fit_generator에서 할 것)
+    train_generator = idg.flow(x_train,y_train,batch_size=8) #훈련데이터셋을 제공할 제네레이터를 지정
+    valid_generator = idg2.flow(x_valid,y_valid) # validation_data에 넣을 것
+    test_generator = idg2.flow(test2,shuffle=False)  # predict(x_test)와 같은 역할
+    
+    model = modeling()
+    # mc = ModelCheckpoint('C:/data/modelCheckpoint/0202_3_best_mc.h5', save_best_only=True, verbose=1)
+    model.compile(loss = 'sparse_categorical_crossentropy', optimizer=Adam(lr=0.002,epsilon=None),metrics=['acc']) # y의 acc가 목적
+    img_fit = model.fit_generator(train_generator,epochs=epochs, validation_data=valid_generator, callbacks=[ea,re])
+    
+    # predict
+    # model.load_weights('C:/data/modelCheckpoint/0202_3_best_mc.h5')
+    result += model.predict_generator(test_generator,verbose=True)/40 #a += b는 a= a+b
+    # predict_generator 예측 결과는 클래스별 확률 벡터로 출력
+    print('result:', result)
 
-# inputs2 = Input(shape=(785,1))
-# bn = BatchNormalization(trainable=False)(inputs2)                                      # 배치정규화
-# conv = Conv1D(128, kernel_size=5, strides=1, padding='same', activation='relu')(bn)   # CNN
-# bn = BatchNormalization()(conv)                                                       # 배치정규화
-# pool = Conv1D(128, kernel_size=2, strides=1, padding='same', activation='relu')(bn)   # CNN
-# flatten = Flatten()(pool)
-# dense2 = Dense(128, activation='relu')(flatten)
+    # save val_loss
+    hist = pd.DataFrame(img_fit.history)
+    val_loss_min.append(hist['val_loss'].min())
+    nth += 1
+    print(nth, 'set complete!!') # n_splits 다 돌았는지 확인
 
-# from tensorflow.keras.layers import concatenate, Concatenate
-# merge1 = concatenate([dense1, dense2])
-
-flatten = Flatten()(pool)                                                             # Flatten
-bn = BatchNormalization()(flatten)                                                    # 배치정규화
-dense = Dense(1000, activation='relu')(bn)                                            # Fully Connected Layer
-bn = BatchNormalization()(dense)                                                      # 배치정규화
-outputs = Dense(10, activation='softmax')(bn)                                         # Fully Connected Layer
-
-model = Model(inputs=inputs1, outputs=outputs)
-
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=500, validation_split=0.2,)
-loss, acc = model.evaluate(x_test, y_test, batch_size=32)
-print(loss, acc)
-x_pred = test.drop(['id', 'letter'], axis=1).values
-x_pred = x_pred.reshape(-1, 28, 28, 1)
-
-submission = pd.read_csv('./csv/submission.csv')
-submission['digit'] = np.argmax(model.predict(x_pred), axis=1)
-submission.to_csv('baseline.csv', index=False)'''
+print(val_loss_min)
+model.summary()
+#제출========================================
+sub = pd.read_csv('./csv/submission.csv')
+sub['digit'] = result.argmax(1) # y값 index 2번째에 저장
+sub.to_csv('./0202_5_mnist.csv',index=False)
--- model/1. vision_5.py
+++ model/1. vision_5.py
@@ -7,14 +7,38 @@ import tensorflow as tf
 
 train = pd.read_csv('./csv/train.csv')
 test = pd.read_csv('./csv/test.csv')
+x_pred = test.drop(['id', 'letter'], axis=1).values
 
 x_train = train.drop(['id', 'digit', 'letter'], axis=1).values
-x_train = x_train.reshape(-1, 28, 28, 1)
+# x_train = x_train.reshape(-1, 28, 28, 1)
 x = x_train/255
 y = train['digit']
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.decomposition import PCA
+
+# pca = PCA()
+# pca.fit(x)
+# cumsum = np.cumsum(pca.explained_variance_ratio_)
+# print('cumsum : ', cumsum)
+
+# d = np.argmax(cumsum>0.98)+1
+# print('cumsum >= 0.98', cumsum>=0.98)
+# print('d : ', d) # 205
+
+pca =PCA(n_components = 196)
+pca.fit(x)
+x = pca.transform(x)
+x_pred = pca.transform(x_pred)
+from xgboost import XGBClassifier,plot_importance
+x = x.reshape(-1, 14, 14, 1)
+x_pred = x_pred.reshape(-1, 14, 14, 1)
+
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test= train_test_split(x, y, train_size = 0.8, shuffle = True, random_state = 66 )
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8, shuffle = True, random_state = 66 )
+
+# from sklearn.model_selection import train_test_split
+# x_train, x_test, y_train, y_test= train_test_split(x, y, train_size = 0.8, shuffle = True, random_state = 66 )
 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 encoder = OneHotEncoder()
@@ -27,7 +51,7 @@ y_test = encoder.fit_transform(y_test.values.reshape(-1,1)).toarray()
 
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout, Conv1D, MaxPool1D, LSTM, Input, BatchNormalization,MaxPooling2D
-inputs1 = Input(shape=(28,28,1))
+inputs1 = Input(shape=(14,14,1))
 bn = BatchNormalization(trainable=False)(inputs1)                                      # 배치정규화
 conv = Conv2D(128, kernel_size=5, strides=1, padding='same', activation='relu')(bn)   # CNN
 bn = BatchNormalization()(conv)                                                       # 배치정규화
@@ -55,11 +79,9 @@ outputs = Dense(10, activation='softmax')(bn)                                   
 model = Model(inputs=inputs1, outputs=outputs)
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=500, validation_split=0.2,)
+model.fit(x_train, y_train, epochs=500, validation_split=0.2)
 loss, acc = model.evaluate(x_test, y_test, batch_size=32)
 print(loss, acc)
-x_pred = test.drop(['id', 'letter'], axis=1).values
-x_pred = x_pred.reshape(-1, 28, 28, 1)
 
 submission = pd.read_csv('./csv/submission.csv')
 submission['digit'] = np.argmax(model.predict(x_pred), axis=1)
